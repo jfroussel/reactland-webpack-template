d58cf50d5bfae49ccd681d51bf328b01eeab48b7
jeff <jeff@jeffHome.lan>
2019-01-30 12:33:17 +0100

    wip

---
+++ src/index.js
@@ -0,0 +1,5 @@
+    (function () {
+        require("../src/assets/stylesheets/styles.scss")
+        let a = "welcome on template page"
+        document.write(a)
+    })()
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require("webpack")
+const path = require("path")
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin")
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const OptimizeCSSAssets = require("optimize-css-assets-webpack-plugin")
+const DashboardPlugin = require("webpack-dashboard/plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let config = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./public"),
+        filename: "./bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.scss$/,
+            use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader', 'postcss-loader'],
+            }))
+        }
+        ]
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin("styles.css"),
+        new DashboardPlugin(),
+        new HtmlWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    devServer: {
+        contentBase: path.resolve(__dirname, "./public"),
+        historyApiFallback: true,
+        inline: true,
+        open: true,
+        hot: true
+    },
+    devtool: "eval-source-map"
+}
+
+module.exports = config
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new UglifyJSPlugin(),
+        new OptimizeCSSAssets()
+    );
+}
